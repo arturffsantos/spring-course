88ebe0555376d78de60983f5277ef7088308e33f
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-03-28 22:14:15 -0300

    create request stage repository

---
+++ src/main/java/com/springcourse/repository/RequestStageRepository.java
@@ -0,0 +1,12 @@
+package com.springcourse.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springcourse.domain.RequestStage;
+
+public interface RequestStageRepository extends JpaRepository<RequestStage, Long>{
+	
+	public List<RequestStage> findAllByRequest(Long id);
+}
