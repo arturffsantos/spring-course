806b54f1081c9c73c7958a8653d9c583fa8e631a
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-04-01 23:12:19 -0300

    add annotations @Transational and @MOdifying in updateStatus

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -2,7 +2,10 @@ package com.springcourse.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.springcourse.domain.Request;
@@ -12,6 +15,8 @@ public interface RequestRepository extends JpaRepository<Request, Long>{
 	
 	public List<Request> findAllByOwnerId(Long id);
 	
+	@Transactional
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
-	public Request updateStatus(Long id, RequestState state);
+	public int updateStatus(Long id, RequestState state);
 }
