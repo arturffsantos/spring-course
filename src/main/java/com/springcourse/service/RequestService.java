31f0252e1f03d8dc543734ee7aee3f4b6c4b3de0
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-04-04 20:44:10 -0300

    create request service

---
+++ src/main/java/com/springcourse/service/RequestService.java
@@ -0,0 +1,37 @@
+package com.springcourse.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.enums.RequestState;
+import com.springcourse.repository.RequestRepository;
+
+public class RequestService {
+	@Autowired private RequestRepository requestRepository;
+	
+	public Request save(Request request) {
+		request.setState(RequestState.OPEN);
+		request.setCreationDate(new Date());
+		
+		return requestRepository.save(request);
+	}
+	
+	public Request update(Request request) {
+		return requestRepository.save(request);
+	}
+	
+	public Request findById(Long id) {
+		return requestRepository.findById(id).get();
+	}
+
+	public List<Request> listAll(){
+		return requestRepository.findAll();
+	}
+	
+	public List<Request> listAllByOwnerId(Long id){
+		return requestRepository.findAllByOwnerId(id);
+	}
+}
