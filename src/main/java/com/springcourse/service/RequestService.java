93c4f8dc75eb62037389db6d12c3808338521ed1
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-04-04 20:49:39 -0300

    fix: add service annotation

--- src/main/java/com/springcourse/service/RequestService.java
+++ src/main/java/com/springcourse/service/RequestService.java
@@ -4,11 +4,13 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.springcourse.domain.Request;
 import com.springcourse.domain.enums.RequestState;
 import com.springcourse.repository.RequestRepository;
 
+@Service
 public class RequestService {
 	@Autowired private RequestRepository requestRepository;
 	
